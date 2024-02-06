e3114edce407d736961a94f63e322950b4b5d021
Sheeyx <sheeyx00@gmail.com>
2024-02-06 14:19:27 +0900

    BRR: 19-EJS framework building frontend side

--- server.js
+++ server.js
@@ -16,13 +16,15 @@ app.set("views","views");
 app.set("view engine","ejs");
 
 //4 - Routing code
+app.post("/create-item", (req,res)=>{
+    console.log(req.body);
+    res.json({test: "success"});
+})
+
 app.get("/", function(req,res){
-    res.end("<h1>Hello World</h1>");
+    res.render("harid");
 });
 
-app.get("/gift", function(req,res){
-    res.end("<h1>Siz sovgalar bolimidasiz</h1>");
-});
 
 const server = http.createServer(app);
 let PORT = 3000;
